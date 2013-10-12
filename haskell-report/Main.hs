7a68d3bea18543c4693340f910f77a755b970090
Anders Jellinggaard <anders.jel@gmail.com>
2013-10-12 20:08:16 +0200

    Options as functions

--- haskell-report/Main.hs
+++ haskell-report/Main.hs
@@ -6,54 +6,88 @@ import           Control.Applicative
 import           Control.Monad                  (unless)
 import qualified Data.Aeson                     as Aeson
 import           Data.ByteString.Lazy           (ByteString, hPut)
-import           Data.Foldable
+import           Data.Default
+import           Data.Foldable                  (for_)
+import           Data.Lens
+import           Data.Maybe                     (fromMaybe)
 import           HaskellReport.Extract          (extract)
 import qualified HaskellReport.Interp           as Interp
 import           System.Console.GetOpt          (ArgDescr (..), OptDescr (..))
 import qualified System.Console.GetOpt          as GetOpt
 import           System.Environment             (getArgs)
-import           System.Exit                    (exitFailure, ExitCode(..))
-import qualified System.IO as IO
+import           System.Exit                    (ExitCode (..), exitFailure)
+import qualified System.IO                      as IO
 import           System.Process.ByteString.Lazy (readProcessWithExitCode)
 import           Text.Pandoc                    (Pandoc)
 import           Text.Report.Types              (Report, runReport)
 
-data Flag =
-    GhcOptions [String]
-    | PandocExe FilePath
-    | PandocOptions [String]
-    | FromFormat String
-    | ToFormat String
-    | OutputFile FilePath
-
 data Arguments = Arguments
-    { inputFile :: FilePath
-    , flags     :: [Flag]
+    { ghcOptions    :: [String]
+    , inputFile     :: FilePath
+    , inputFormat   :: Maybe String
+    , outputFile    :: Maybe FilePath
+    , outputFormat  :: Maybe String
+    , pandocExe     :: FilePath
+    , pandocOptions :: [String]
     }
 
-options :: [OptDescr Flag]
+lGhcOptions :: Lens Arguments [String]
+lGhcOptions = lens ghcOptions (\x y -> y{ghcOptions = x})
+lInputFile :: Lens Arguments FilePath
+lInputFile = lens inputFile (\x y -> y{inputFile = x})
+lInputFormat :: Lens Arguments (Maybe String)
+lInputFormat = lens inputFormat (\x y -> y{inputFormat = x})
+lOutputFile :: Lens Arguments (Maybe FilePath)
+lOutputFile = lens outputFile (\x y -> y{outputFile = x})
+lOutputFormat :: Lens Arguments (Maybe String)
+lOutputFormat = lens outputFormat (\x y -> y{outputFormat = x})
+lPandocExe :: Lens Arguments FilePath
+lPandocExe = lens pandocExe (\x y -> y{pandocExe = x})
+lPandocOptions :: Lens Arguments [String]
+lPandocOptions = lens pandocOptions (\x y -> y{pandocOptions = x})
+
+instance Default Arguments where
+    def = Arguments
+        { ghcOptions = def
+        , inputFile = def
+        , inputFormat = def
+        , outputFile = def
+        , outputFormat = def
+        , pandocExe = "pandoc"
+        , pandocOptions = def
+        }
+
+options :: [OptDescr (Arguments -> Arguments)]
 options =
-    [ Option "g" ["ghc-option"] (ReqArg (GhcOptions . pure) "OPTION")
+    [ Option "g" ["ghc-option"]
+        (ReqArg (\s -> modL lGhcOptions (++[s])) "OPTION")
         "An option for GHC"
-    , Option "G" ["ghc-options"] (ReqArg (GhcOptions . words) "OPTIONs")
+    , Option "G" ["ghc-options"]
+        (ReqArg (\ss -> modL lGhcOptions (++words ss)) "OPTIONs")
         "Several options for GHC (split on spaces)"
-    , Option "p" ["pandoc-option"] (ReqArg (PandocOptions . pure) "OPTION")
+    , Option "p" ["pandoc-option"]
+        (ReqArg (\s -> modL lPandocOptions (++[s])) "OPTION")
         "An option for pandoc"
-    , Option "P" ["pandoc-options"] (ReqArg (PandocOptions . words) "OPTIONs")
+    , Option "P" ["pandoc-options"]
+        (ReqArg (\ss -> modL lPandocOptions (++words ss)) "OPTIONs")
         "Several options for pandoc (split on spaces)"
-    , Option "e" ["pandoc-path"] (ReqArg PandocExe "EXEC")
+    , Option "e" ["pandoc-path"]
+        (ReqArg (setL lPandocExe) "EXEC")
         "Where to find the pandoc executeable"
-    , Option "f" ["--from"] (ReqArg FromFormat "FORMAT")
+    , Option "f" ["--from"]
+        (ReqArg (setL lInputFormat . Just) "FORMAT")
         "Input format (forwarded to pandoc)"
-    , Option "t" ["--to"] (ReqArg ToFormat "FORMAT")
+    , Option "t" ["--to"]
+        (ReqArg (setL lOutputFormat . Just) "FORMAT")
         "Output format (forwarded to pandoc)"
-    , Option "o" ["--output"] (ReqArg OutputFile "FILE")
+    , Option "o" ["--output"]
+        (ReqArg (setL lOutputFile . Just) "FILE")
         "Output file (or - for stdout)"
     ]
 
 getArguments :: IO Arguments
 getArguments = do
-    (flags', args, errors) <- GetOpt.getOpt GetOpt.Permute options <$> getArgs
+    (flags, args, errors) <- GetOpt.getOpt GetOpt.Permute options <$> getArgs
     let header = "usage: haskell-report [OPTIONS] infile"
         info = GetOpt.usageInfo header options
         bail = putStrLn info >> exitFailure
@@ -61,44 +95,8 @@ getArguments = do
         for_ errors putStrLn
         bail
     case args of
-        [inputFile'] -> return $ Arguments inputFile' flags'
+        [inputFile'] -> return $ foldl (flip id) def{inputFile = inputFile'} flags
         _ -> bail
-
-getGhcArgs :: Arguments -> [String]
-getGhcArgs args = do
-    f <- flags args
-    case f of
-        GhcOptions os -> os
-        _ -> []
-
-data PandocStage = PandocRead | PandocWrite
-
-getPandocArgs :: Arguments -> PandocStage -> [String]
-getPandocArgs args stage = do
-    f <- flags args
-    case f of
-        PandocOptions os -> os
-        ToFormat x -> case stage of
-            PandocWrite -> ["-t", x]
-            PandocRead -> []
-        FromFormat x -> case stage of
-            PandocWrite -> []
-            PandocRead -> ["-f", x]
-        _ -> []
-
-getPandocExe :: Arguments -> String
-getPandocExe args = g $ flags args
-  where
-    g [] = "pandoc"
-    g (PandocExe exe : _) = exe
-    g (_ : fs) = g fs
-
-getOutputFile :: Arguments -> String
-getOutputFile args = g $ flags args
-  where
-    g [] = "-"
-    g (OutputFile path : _) = path
-    g (_ : fs) = g fs
 
 callProcess :: FilePath -> [String] -> ByteString -> IO ByteString
 callProcess exe args stdin = do
@@ -110,9 +108,11 @@ callProcess exe args stdin = do
 
 readDoc :: Arguments -> IO Pandoc
 readDoc args = do
-    let args' = getPandocArgs args PandocRead
+    let args' =
+            pandocOptions args
+            ++ maybe [] (\f -> ["-f", f]) (inputFormat args)
             ++ ["-t", "json", inputFile args]
-    output <- callProcess (getPandocExe args) args' ""
+    output <- callProcess (pandocExe args) args' ""
     case Aeson.decode' output of
         Nothing -> do
             putStrLn "Unexpected output from pandoc:"
@@ -122,16 +122,19 @@ readDoc args = do
 
 writeDoc :: Arguments -> Pandoc -> IO ()
 writeDoc args doc = do
-    let args' = getPandocArgs args PandocWrite
-            ++ ["-f", "json", "-o", getOutputFile args, "-"]
-    output <- callProcess (getPandocExe args) args' $ Aeson.encode doc
+    let args' =
+            pandocOptions args
+            ++ maybe [] (\t -> ["-t", t]) (outputFormat args)
+            ++ ["-o", fromMaybe "-"  $ outputFile args]
+            ++ ["-f", "json", "-"]
+    output <- callProcess (pandocExe args) args' $ Aeson.encode doc
     hPut IO.stdout output
 
 extractReport :: Arguments -> Pandoc -> IO (Report Pandoc)
 extractReport args doc = do
     let interpOptions = Interp.Options
             { Interp.inputFile = inputFile args
-            , Interp.ghcArgs   = getGhcArgs args
+            , Interp.ghcArgs   = ghcOptions args
             }
     Interp.runInterp interpOptions $ extract doc
 
