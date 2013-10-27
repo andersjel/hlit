776fc2784e3891c2dc78e9a223ab8909476275d1
Anders Jellinggaard <anders.jel@gmail.com>
2013-10-27 14:03:26 +0100

    Worked on splices.

--- hlit/Lit/Splice.hs
+++ hlit/Lit/Splice.hs
@@ -1,19 +1,56 @@
-module Lit.Splice where
+module Lit.Splice
+    ( Splice
+    , Expr (..)
+    , Error
+    , Options (..)
+    , splice
+    , runSplice
+    ) where
 
-data Splice
+import           Control.Applicative
+import           Control.Monad.Error        (ErrorT)
+import qualified Control.Monad.Error        as Err
+import           Control.Monad.State.Strict (State)
+import qualified Control.Monad.State.Strict as State
+import qualified Data.Aeson                 as Aeson
+import           Data.Monoid
+import           Data.Sequence              (Seq)
+import qualified Data.Sequence              as Seq
+import qualified Language.Haskell.Exts      as H
+
+data Splice a = Splice
+    (Seq Expr) -- ^ The expressions that need to be compiled
+    (Load a)   -- ^ Monad for loading the result of running each expression
 
 instance Functor Splice where
-    fmap = undefined
+    fmap f (Splice s a) = Splice s (f <$> a)
 
 instance Applicative Splice where
-    pure  = undefined
-    (<*>) = undefined
+    pure = Splice mempty . pure
+    Splice sx x <*> Splice sy y = Splice (sx <> sy) (x <*> y)
 
-type TypeID = ()
-type Expr = ()
-type Errors = ()
-type Options = ()
+data Expr = Expr H.Type String
+data Error = CommunicationError String | MiscError String
+    deriving (Show)
+data Options = Options
+    { inputFilePath :: Maybe FilePath
+    , inputContent  :: String
+    }
 
-splice :: TypeID -> Expr -> Splice a
+instance Err.Error Error where
+    noMsg = Err.strMsg "Unknown error"
+    strMsg = MiscError
 
-runSplice :: Options -> Splice a -> IO (Either Errors a)
+type Load = ErrorT Error (State [Aeson.Value])
+
+splice :: Aeson.FromJSON a => Expr -> Splice a
+splice expr = Splice (Seq.singleton expr) $ do
+    (v:vs) <- State.get
+    State.put vs
+    case Aeson.fromJSON v of
+        Aeson.Success x -> return x
+        Aeson.Error x -> Err.throwError $ CommunicationError x
+
+runSplice :: Options -> Splice a -> IO (Either Error a)
+runSplice = undefined
+
