779da9344caf3c7577ddf9da375c78ad8cb69d65
Anders Jellinggaard <anders.jel@gmail.com>
2014-10-02 19:56:40 +0200

    Simplified the types of the table EDSL.

--- hlit-types/src/Text/Lit/Table.hs
+++ hlit-types/src/Text/Lit/Table.hs
@@ -6,56 +6,45 @@ module Text.Lit.Table
     , (#)
     , alignLeft, alignRight, alignCenter
     , width
-    , toPandocTable
     ) where
 
 import           Control.Applicative
 import           Data.Foldable       (toList, Foldable)
-import           Data.Monoid
 import           Data.Traversable    (traverse)
 import           Text.Lit.Render     (Render (..), RenderBlock (..))
 import           Text.Lit.Report     (Report)
 import qualified Text.Pandoc.Builder as Pandoc
 
-data Table t = Table [t] (Report Pandoc.Inlines) [Column t]
+type Table = Report Pandoc.Block
 data Column t = Column
-    { _cells     :: t -> Report Pandoc.TableCell
+    { _format    :: t -> Report Pandoc.TableCell
     , _header    :: Report Pandoc.TableCell
     , _width     :: Double
     , _alignment :: Pandoc.Alignment
     }
 
-toPandocTable :: Table t -> Report Pandoc.Block
-toPandocTable (Table es caption cs)
+table :: (Render a, Foldable f) => a -> [Column t] -> f t -> Table
+table caption columns records
     = Pandoc.Table
-        <$> (toList <$> caption)
-        <*> pure (map _alignment cs)
-        <*> pure (map _width cs)
-        <*> traverse _header cs
-        <*> traverse toRow es
+        <$> (toList <$> render caption)
+        <*> pure (map _alignment columns)
+        <*> pure (map _width columns)
+        <*> traverse _header columns
+        <*> traverse toRow (toList records)
   where
-    toRow e = traverse (\c -> _cells c $ e) cs
-
-instance RenderBlock (Table t) where
-    renderBlock = renderBlock . toPandocTable
-
-def :: Column t
-def = Column undefined undefined 0 Pandoc.AlignDefault
-
-table :: (Render a, Foldable f) => a -> [Column t] -> f t -> Table t
-table caption columns elements
-    = Table (toList elements) (render caption) columns
+    toRow record = traverse (\c -> _format c $ record) columns
 
 inline :: Render a => a -> Report Pandoc.Blocks
 inline x = Pandoc.plain <$> render x
 
 col :: (Render a, Render b) => b -> (t -> a) -> Column t
-col h f = blockCol (inline h) (inline . f)
+col header f = blockCol (inline header) (inline . f)
 
 blockCol :: (RenderBlock a, RenderBlock b) => b -> (t -> a) -> Column t
-blockCol h f = def
-    { _cells=(\e -> toList <$> renderBlock (f e))
-    , _header=toList <$> renderBlock h
+blockCol header f = Column
+    { _format=(\e -> toList <$> renderBlock (f e))
+    , _header=toList <$> renderBlock header
+    , _width=0, _alignment=Pandoc.AlignDefault
     }
 
 alignLeft, alignRight, alignCenter :: Column t -> Column t
